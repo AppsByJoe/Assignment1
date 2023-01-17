be8a049152ae597533ca4eb03675330edd2a6cb3
AppsByJoe <jth0124@auburn.edu>
2023-01-17 09:33:47 -0500

    Initial Commit

---
+++ 5330_Assignment1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  5330_Assignment1
+//
+//  Created by Hronek Joseph on 1/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
